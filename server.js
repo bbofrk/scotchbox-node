70ac145e6cd19c67254d16dfa94aaf73ce2b83d5
bbofrk <mingliwanglau@gmail.com>
2015-04-24 07:42:45 +1200

    express

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("Wow ! nodemon");
+// grab express
+var express = require('express');
+
+// create an express app
+var app = express();
+
+//create an express route for the home page
+// http://localhot:8080
+app.get('/', function(req, res){
+   res.sendfile('index.html');
+});
+// start the server on port 8080
+app.listen(8080);
+//send a message
+console.log('server has started');
