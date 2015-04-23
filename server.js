e0a80f7bcdd9a449bd7f15bfef3b5408678922b4
bbofrk <mingliwanglau@gmail.com>
2015-04-24 08:01:01 +1200

    init instagram app

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ var express = require('express');
 
 // create an express app
 var app = express();
+var ig = require('instagram-node').instagram();
 
+//Configuring
+// tell node where to look for site resources
+app.use(express.static(__dirname + '/public'));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 //create an express route for the home page
 // http://localhot:8080
 app.get('/', function(req, res){
-   res.sendfile('index.html');
+   //render the home page and pass int he popular images
+   res.render('pages/index');
+   //res.sendfile('index.html');
 });
 // start the server on port 8080
 app.listen(8080);
